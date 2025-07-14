ef8db1f6d2a96b3319552c3fedee45b05c1e5e77
Cendikia Ishmatuka <85205431+cencencendi@users.noreply.github.com>
2025-07-14 18:42:04 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -25,7 +25,7 @@ def setup_chrome_driver():
     chrome_options = Options()
     temp_data = tempfile.mkdtemp()
     chrome_options.add_argument(f"--user-data-dir={temp_data}")
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
